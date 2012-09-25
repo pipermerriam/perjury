062408189d2805d692eeb072ac84568bd5f132ea
Aaron Merriam <amerriam@fusionbox.com>
2012-09-24 18:56:30 -0600

    fixed broken imports

--- pyrite/django.py
+++ pyrite/django.py
@@ -1,6 +1,5 @@
-from pyrite import (BaseGenerator, UsernameGenerator, SimplePasswordGenerator,
+from pyrite.generators import (BaseGenerator, UsernameGenerator, SimplePasswordGenerator,
         EmailAddressGenerator)
-from django.contrib.auth.models import User
 
 
 class ModelGenerator(BaseGenerator):
@@ -34,24 +33,25 @@ class ModelGenerator(BaseGenerator):
         instance = self.model.oblects.create(**kwargs)
         return instance
 
+#from django.contrib.auth.models import User
 
-class UserGenerator(ModelGenerator):
-    """
-    Not quite here yet.
-    """
-    fields = (
-            ('username', UsernameGenerator),
-            ('password', SimplePasswordGenerator),
-            ('email', EmailAddressGenerator),
-            )
-    model = User
-
-    def __init__(self, *args, **kwargs):
-        super(UserGenerator, self).__init__(*args, **kwargs)
-        self.name_generator = FullNameGenerator(self.size).__iter__()
-
-    def create_model(self, **kwargs):
-        user = User.objects.create_user(**kwargs)
-        user.first_name, user.last_name = self.name_generator.next()
-        user.save()
-        return user
+#class UserGenerator(ModelGenerator):
+#    """
+#    Not quite here yet.
+#    """
+#    fields = (
+#            ('username', UsernameGenerator),
+#            ('password', SimplePasswordGenerator),
+#            ('email', EmailAddressGenerator),
+#            )
+#    model = User
+#
+#    def __init__(self, *args, **kwargs):
+#        super(UserGenerator, self).__init__(*args, **kwargs)
+#        self.name_generator = FullNameGenerator(self.size).__iter__()
+#
+#    def create_model(self, **kwargs):
+#        user = User.objects.create_user(**kwargs)
+#        user.first_name, user.last_name = self.name_generator.next()
+#        user.save()
+#        return user
